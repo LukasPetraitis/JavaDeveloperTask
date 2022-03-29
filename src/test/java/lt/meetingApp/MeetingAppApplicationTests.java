47a319cff285536ac843d0c51636b7a649a3b8d8
Lukas <lukaspetraitis90@gmail.com>
2022-03-29 21:11:56 +0300

    initialized SpringBoot project

---
+++ src/test/java/lt/meetingApp/MeetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.meetingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
